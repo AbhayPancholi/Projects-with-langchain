66a47789bf00f9110b423a0c8d3b96fae96e4549
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-11-15 14:40:44 +0530

    Added file for a chatbot

---
+++ Chatbot/app.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms.ollama import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an helpful assistant. Please respond to the user queries."),
+        ("user", "Question:{question}"),
+    ]
+)
+
+st.title("Langchain demo with Ollama")
+input_text = st.text_input("Search the topic you want")
+
+llm = Ollama(model="phi")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
