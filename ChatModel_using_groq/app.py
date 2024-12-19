c55827425fd3b1a0b385fdee513dea972fe3274c
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-12-19 19:05:13 +0530

    Added file with complete code for chat model using groq API

--- ChatModel_using_groq/app.py
+++ ChatModel_using_groq/app.py
@@ -1,9 +1,66 @@
 import streamlit as st
 import os
-import langchain
+from langchain_groq import ChatGroq
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrival_chain
+from langchain.chains import create_retrieval_chain
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+groq_api_key = os.environ["GROQ_API_KEY"]
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings = OllamaEmbeddings(model="phi")
+    st.session_state.loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs = st.session_state.loader.load()
+
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200
+    )
+    st.session_state.final_docs = st.session_state.text_splitter.split_documents(
+        st.session_state.docs[:50]
+    )
+    st.session_state.vectors = FAISS.from_documents(
+        st.session_state.final_docs, st.session_state.embeddings
+    )
+
+st.title("Chat wiht groq")
+
+llm = ChatGroq(model="mixtral-8x7b-32768")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
+)
+
+documents = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retriever_chain = create_retrieval_chain(retriever, documents)
+
+input = st.text_input("Ask something related to langsmith")
+
+if input:
+    start = time.process_time()
+    response = retriever_chain.invoke({"input": input})
+    print("Response time: ", start - time.process_time())
+    st.write(response["answer"])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
